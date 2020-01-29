f77085a4b2fcf23deed28b3c3790d38dfb96727f
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-01-29 09:15:39 +0300

    add CURL_3

--- test_data/api_variables_data.py
+++ test_data/api_variables_data.py
@@ -5,3 +5,5 @@ REST_POST = 'http://users.bugred.ru/tasks/rest/list'
 DO_LOGIN = 'http://users.bugred.ru/tasks/rest/dologin?email={}&password={}'
 CURL = 'curl -X POST --header "Content-Type: application/json" "http://users.bugred.ru/tasks/rest/doregister?email=first_mail_1@mail.ru&name=five&password=test"'
 CURL_2 = 'curl -i -X POST -H "Content-Type: application/json" -d "{\"email\": \"mailchik@gmail.com\", \"name\": \"Marmelad\", \"password\": \"1\"}" http://users.bugred.ru/tasks/rest/doregister'
+CURL_3 = 'curl -d "{\"email\": \"mailchik1@gmail.com\", \"name\": \"Marmelad1\", \"password\": \"1\"}" http://users.bugred.ru/tasks/rest/doregister'
+
