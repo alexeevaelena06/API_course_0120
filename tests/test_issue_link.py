a1fca7ae1ecb7d920f90ad3d7a7f554e4cb5fdde
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-02-19 09:45:16 +0300

    update test_issue_link.py (headers checking)

--- tests/test_issue_link.py
+++ tests/test_issue_link.py
@@ -6,6 +6,28 @@ from requests.auth import HTTPBasicAuth
 import json
 from test_data.api_variables_data import GET_ISSUE_LINK, CREATE_ISSUE_LINK
 
+# Korneev's code
+
+
+def test_dz_3():
+    JIRA_EMAIL = "mail.for.testbase@yandex.ru"
+    JIRA_TOKEN = "8aVwgGTTJNrBiel5TUnX3229"
+    BASE_URL = "http://testbase.atlassian.net"
+    API_URL = "/rest/api/3/issue/TEST-4089"
+    API_URL = BASE_URL + API_URL
+    BASIC_AUTH = HTTPBasicAuth(JIRA_EMAIL, JIRA_TOKEN)
+    HEADERS = {'Content-Type': 'application/json',
+               'Referrer Policy': 'strict-origin'}
+    PARAMETERS = {'fields': '*all'}
+    response = requests.get(API_URL, headers=HEADERS, params=PARAMETERS, auth=BASIC_AUTH)
+    assert response.status_code == 200, f'API не доступна error code {response.status_code}'
+    if 'Referer' in response.headers:
+        assert BASE_URL in response.headers['Referer'], \
+            'При запросе issue через API произошла переадресация запроса на внешний ресурс'
+    assert 'Proxy-Authenticate' not in response.headers.keys(), \
+        f'Используется прокси {response.headers["Proxy-Authenticate"]}'
+
+
 """CreateIssueLink"""
 
 
@@ -43,12 +65,12 @@ def test_create_issue():
 
 def test_headers():
     # "Проверяем доступ к issue"
-    url = GET_ISSUE_LINK.format(10200)
+    url = GET_ISSUE_LINK.format(12232)
     auth = HTTPBasicAuth("mail.for.testbase@yandex.ru", "cCehOzIeIcisnqaCzBKQ1E53")
     headers = {"Accept": "application/json"}
     response = requests.request("GET", url, headers=headers, auth=auth)
     print(json.dumps(json.loads(response.text, encoding='utf-8'), sort_keys=True, indent=4, separators=(",", ": ")))
-    # assert response.status_code == 200, f"Получен статус код: {response.status_code} != 200"
+    assert response.status_code == 200, f"Получен статус код: {response.status_code} != 200"
     # "Проверяем headers ответа"
     assert response.headers['Content-Type'] == 'application/json;charset=UTF-8', \
         f"Заголовок Content-Type = {response.headers['Content-Type']} вместо 'application/json;charset=UTF-8'"
@@ -69,4 +91,3 @@ def test_headers():
         f"{response.request.headers['Accept']} != 'application/json'"
     assert response.request.headers['Connection'] == 'keep-alive', \
         f"{response.request.headers['Connection']} != 'keep-alive'"
-    # print("referrer:", response.request.headers['referrer'])
