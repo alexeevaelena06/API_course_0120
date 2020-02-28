55e42449bd0d5c10263e17430efadc1ed63ec25e
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-02-28 10:13:06 +0300

    update test_send_avatar.py

--- test_data/api_variables_data.py
+++ test_data/api_variables_data.py
@@ -15,7 +15,7 @@ CURL_2 = 'curl -i -X POST -H "Content-Type: application/json" -d "{\"email\": \"
 CURL_3 = 'curl -d "{\"email\": \"mailchik1@gmail.com\", \"name\": \"Marmelad1\", \"password\": \"1\"}" http://users.bugred.ru/tasks/rest/doregister'
 GET_ISSUE_LINK = 'https://testbase.atlassian.net/rest/api/2/issueLink/{}'
 CREATE_ISSUE_LINK = 'https://testbase.atlassian.net/rest/api/2/issueLink'
-ADD_AVATAR = 'http://users.bugred.ru/tasks/rest/addavatar/?email={email}'
+ADD_AVATAR = 'http://users.bugred.ru/tasks/rest/addavatar/?email={}'
 
 """DoRegister"""
 
--- tests/test_send_avatar.py
+++ tests/test_send_avatar.py
@@ -11,21 +11,21 @@ from test_data.api_variables_data import DO_REGISTER_REST, data_user, ADD_AVATAR
 faker = Faker()
 
 
-@pytest.mark.parametrize("email, name, password", data_user())
-@pytest.mark.parametrize("file", [(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Python.jpg')), (''),
-                                  ('adfdfdf')])
+# @pytest.mark.parametrize("email, name, password", data_user())
+@pytest.mark.parametrize("email, name, password", [("qq2@gmail.com", "Jill", "fsyise")])
+@pytest.mark.parametrize("file", [(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Python.jpg'))])
 def test_send_avatar(email, name, password, file):
-    print(f"Новый пользователь email: {email}, name: {name}, password: {password}")
-    doregister = requests.post(url=DO_REGISTER_REST.format(email, name, password))
-    print(doregister.text.encode('utf-8-sig'))
-    assert doregister.text.encode('utf-8-sig'), f'Refused request. Server answer {doregister}'
-    url = ADD_AVATAR.format(email=email)
+    # print(f"Новый пользователь email: {email}, name: {name}, password: {password}")
+    # doregister = requests.post(url=DO_REGISTER_REST.format(email, name, password))
+    # print(doregister.text.encode('utf8').decode("utf8"))
+    # assert doregister.text.encode('utf8'), f'Refused request. Server answer {doregister}'
+    url = ADD_AVATAR.format(email)
     payload = {}
     files = [
       ('avatar', open(file, 'rb'))
     ]
-    headers = {
-      'Content-Type': 'multipart/form-data; boundary=--------------------------846395766302543056450567'
-    }
+    headers = {'Content-Type': 'multipart/form-data',
+               'Cookie': 'PHPSESSID=fd7650477ff609ce4e238e764adcb2da'}
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    print(response.text.encode('utf-8-sig'))
+    print(response.text)
+    assert response.json()["status"] == 'ok', f'Error message: {response.text.encode("utf8").decode("utf8")}'
